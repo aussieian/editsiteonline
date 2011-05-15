8a3eb7333afee2c7d3d2a437d0cbc7242136c55b
Ian C macbook <ian@insight4.com>
2011-05-15 12:17:10 +1000

    added contact form for insight4 website

--- yds_lib/i4_contact_form.php
+++ yds_lib/i4_contact_form.php
@@ -2,7 +2,7 @@
 
 // ajax contact form for insight4 website
 
-$mailto = 'info@insight4.com' ;
+$mailto = 'ian@insight4.com' ;
 $subject = "Enquiry from web site" ;
 
 if (isset($_POST['submit'])) {
@@ -10,15 +10,15 @@ if (isset($_POST['submit'])) {
 	// Prepare message
 	$msg = "Time: " . date("m/d/y g:ia", time()) . "\n";
 	foreach ($_POST as $field=>$value) {
-		if ($field != "submit") $msg .= $field . ": " . $value . "\n";
+		if ($field != " submit") $msg .= $field . ": " . $value . "\n";
 	}
 	
 	if (mail($mailto, $subject, $msg, "From: ".$mailto)) {
 		// Email was sent
-		print("<p>Thanks for contacting us! Please allow 1-2 business days for us to respond to your enquiry.</p>")
+		print("<p>Thanks for contacting us! Please allow 1-2 business days for us to respond to your enquiry.</p>");
 	} else {
 		// Erro sending email
-		print("<p>Sorry, there was a problem submitting your enquiry. Please email us at info@insight4.com.</p>")
+		print("<p>Sorry, there was a problem submitting your enquiry. Please email us at info@insight4.com.</p>");
 	}
 }
 
