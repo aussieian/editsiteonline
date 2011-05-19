cf4760f5d66f085d3e084eaf40d5320a195174dd
Ian Cumming <ian+imac@insight4.com>
2011-05-19 15:46:45 +1000

    added OR to html

--- yds_lib/new_page.php
+++ yds_lib/new_page.php
@@ -41,7 +41,7 @@ if (( $real_key != $secret_key) || ($content == ""))
 				<br><div id="attachment_quota"><?php if (domain_is_over_quota($domain)) { print("<span style='color: red;'>Over Quota :(</span>"); } else { print("Quota: "); }?> You are using <?php print(round((get_domain_attachments_size($domain)/1000), 1)); ?>MB of <?php print(round((get_domain_attachments_limit($domain)/1000), 1)); ?>MB <br>(<a target="_blank" style='color: #fff;' href="http://yoodoos.com/#donate">Donate</a> to get more quota.)</div>
 			</div>
 		</div>
-		<div style="clear: both; padding-top: 15px; text-align: left;">Page HTML<br><textarea name='content' class="editcontent"><?php print($content);?></textarea></div>
+		<div style="clear: both; padding-top: 15px; text-align: left;">- OR - Page HTML<br><textarea name='content' class="editcontent"><?php print($content);?></textarea></div>
 		<div style="font-size: 10pt; text-align: left; padding-top: 10px;">
 		</div>
 		<div style="padding-left: 50px; clear: both; padding-top: 15px;"><input type='submit' style="font-size: 12pt; color: #fff;" value='create page &raquo;' class="button orange"></div>
