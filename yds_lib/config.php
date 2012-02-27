125c99c8143bdc6291e5697fc665a111af55c77f
Ian C <ian@insight4.com>
2012-02-28 02:23:31 +1100

    update

--- yds_lib/config.php
+++ yds_lib/config.php
@@ -10,7 +10,7 @@ $password = "editsite0nline";
 $hostname = "localhost";
 $dbname = 'editsiteonline';
 
-$host_names = array("EditSiteOnline.com", "dev.EditSiteOnline.com", "www.EditSiteOnline.com", "mail.EditSiteOnline.com", "admin.EditSiteOnline.com", "yhs.EditSiteOnline.com", "hostme.EditSiteOnline.com", "184.72.55.6", "localhost", "49.156.17.170", "localhost.editsiteonline.com", "www.editsiteonline.com", "dev.editsiteonline.com", "admin.editsiteonline.com");
+$host_names = array("EditSiteOnline.com", "dev.EditSiteOnline.com", "www.EditSiteOnline.com", "mail.EditSiteOnline.com", "admin.EditSiteOnline.com", "yhs.EditSiteOnline.com", "hostme.EditSiteOnline.com", "184.72.55.6", "localhost", "49.156.17.170", "localhost.editsiteonline.com", "www.editsiteonline.com", "dev.editsiteonline.com", "admin.editsiteonline.com", "editsiteonline.com");
 
 // email
 $mail_from = "editsiteonline.com <noreply@editsiteonline.com>";
